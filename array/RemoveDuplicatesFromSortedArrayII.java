455fa8864f2a7411e49c00458f2d142fe0404471
SaraMohmed <saradawaly003@gmail.com>
2024-12-04 15:05:02 +0200

    array topic

---
+++ array/RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,12 @@
+package org.example.array;
+
+public class RemoveDuplicatesFromSortedArrayII {
+    public int removeDuplicates(int[] nums){
+        int  k= 0;
+        for(int i=0;i<nums.length;i++){
+            if(k<2 || nums[i]!=nums[k-2])
+                nums[k++] = nums[i];
+        }
+        return k;
+    }
+}
