887355154887d76666eb5c374a864f1b2c031389
SaraMohmed <saradawaly003@gmail.com>
2024-12-05 13:33:47 +0200

    array topic

--- array/MajorityElement.java
+++ array/MajorityElement.java
@@ -1,2 +1,17 @@
-package org.example.array;public class MajorityElement {
+package org.example.array;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums){
+        int c = 0;
+        int majority = nums[0];
+        for(int num : nums){
+            if(c == 0)
+                majority = num;
+            if(majority == num)
+                c++;
+            else
+                c--;
+        }
+        return majority;
+    }
 }
